3819c436f7e2d60ef929a30686df8118870bcb20
Pedro Chambino <pchambino@gmail.com>
2018-12-12 11:22:25 +0000

    Drop Honeykiq#periodic_reporter
    Just use Honeykiq::PeriodicReporter.new

--- spec/honeykiq_spec.rb
+++ spec/honeykiq_spec.rb
@@ -2,11 +2,4 @@ RSpec.describe Honeykiq do
   it 'has a version number' do
     expect(described_class::VERSION).not_to be nil
   end
-
-  describe '#periodic_reporter' do
-    it 'retuns a PeriodicReporter' do
-      expect(described_class.periodic_reporter(honey_client: double))
-        .to be_a(described_class::PeriodicReporter)
-    end
-  end
 end
